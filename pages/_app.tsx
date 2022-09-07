b269f5de7ba4f0efccddcae50e9dd19d1c4ad49d
Benyam Seifu <45682486+benyam7@users.noreply.github.com>
2022-09-07 20:25:35 +0300

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react/react";
 
+const colors = {
+  background: "#1f1f1f",
+  accent: "#833bbe",
+  bodyText: "rgba(255,255,255, 0.75)",
+};
+
+const theme = extendTheme({ colors });
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
